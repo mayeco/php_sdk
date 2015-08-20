574aed21dd3ebf3d0efa375abe80ded7ff8ce372
Mario Young <maye.co@gmail.com>
2015-08-20 19:40:03 -0300

    refactor class

--- lib/Autentifactor.php
+++ lib/Autentifactor.php
@@ -18,24 +18,16 @@ class Autentifactor {
         $this->token = $token;
     }
 
-    /**
-     * Autenticacion de OTC
-     * @return <bool>
-     */
-    public function validateOtc($code, $request_token) {
+    private function request($endpoint, $request_token, $body) {
 
         $client = new Client();
 
-        $body['code'] = $code;
-
         $bearer = 'Bearer ' . $request_token;
 
-        $endpoint = "/v1/users/otc";
-
         $options = [
-            'headers' => ['Authorization' => $bearer],
-            'body' => json_encode($body),
-          ];
+          'headers' => ['Authorization' => $bearer],
+          'body' => json_encode($body),
+        ];
 
         $res = $client->post($this->host . $endpoint, $options);
         // echo $res->getBody();
@@ -49,51 +41,28 @@ class Autentifactor {
             return $auth_token;
         }
     }
+
+    /**
+     * Autenticacion de OTC
+     * @return <bool>
+     */
+    public function validateOtc($code, $request_token) {
+        return $this->request("/v1/users/otc", $request_token, ['code' => $code]);
+    }
+
     /**
      * Delega autenticacion
      * @return <string>
      */
-    public function delegate($email){
-
-        $client = new Client();
-
-        $body['account'] = $email;
-
-        $bearer = 'Bearer ' . $this->token;
-
-        $endpoint = "/v2/users/delegate";
-
-        $options = [
-            'headers' => ['Authorization' => $bearer],
-            'body' => json_encode($body),
-          ];
-
-        $res = $client->post($this->host . $endpoint, $options);
-        // echo $res->getBody();
-        // echo $res->getStatusCode();
-
-        $af_2fa_token = $res->getHeader('x-app-autentifactor');
-
-        return $af_2fa_token;
+    public function delegate($email) {
+        return $this->request("/v2/users/delegate", $this->token, ['account' => $email]);
     }
 
     /**
      * autenticacion
      * @return <string>
      */
-    public function authenticate($username, $password){
-        $client = new GuzzleHttp\Client();
-
-        $body['email'] = $username;
-        $body['password'] = $password;
-
-        $res = $client->post($this->host . "/v1/users/authenticate", [ 'json' => $body ]);
-        // echo $res->getBody();
-        // echo $res->getStatusCode();
-
-        $af_2fa_token = $res->getHeader('x-app-autentifactor');
-
-        return $af_2fa_token;
+    public function authenticate($username, $password) {
+        return $this->request("/v2/users/authenticate", $this->token, ['email' => $username, 'password' => $password]);
     }
-
 }
